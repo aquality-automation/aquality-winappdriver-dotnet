a649b90e80c6b75993b55b77c1af706e170c3c8a
Elena Suboch <korbrakit@gmail.com>
2019-09-13 19:00:52 +0300

    moved out constant

--- Aquality.WinAppDriver/tests/Aquality.WinAppDriver.Tests/Elements/TextBoxTests.cs
+++ Aquality.WinAppDriver/tests/Aquality.WinAppDriver.Tests/Elements/TextBoxTests.cs
@@ -8,34 +8,32 @@ namespace Aquality.WinAppDriver.Tests.Elements
 {
     public class TextBoxTests : TestWithApplication
     {
+        private const string ExpectedValue = "1";
         private IElementFactory Factory => ApplicationManager.GetRequiredService<IElementFactory>();
 
         [Test]
         public void Should_EnterValues()
         {
             var textBox = GetTextBox(Factory, CalculatorWindow.RightArgumentTextBox, "Right Argument");
-            const string expectedValue = "1";
-            textBox.Type(expectedValue);
-            Assert.AreEqual(expectedValue, textBox.Value);
+            textBox.Type(ExpectedValue);
+            Assert.AreEqual(ExpectedValue, textBox.Value);
         }
 
         [Test]
         public void Should_ClearTextBeforeEnteringValues_PrefilledTextbox()
         {
             var textBox = GetTextBox(Factory, CalculatorWindow.RightArgumentTextBox, "Right Argument");
-            const string expectedValue = "1";
             textBox.Type("123");
-            textBox.ClearAndType(expectedValue);
-            Assert.AreEqual(expectedValue, textBox.Value);
+            textBox.ClearAndType(ExpectedValue);
+            Assert.AreEqual(ExpectedValue, textBox.Value);
         }
 
         [Test]
         public void Should_ClearTextBeforeEnteringValues_EmptyTextbox()
         {
             var textBox = GetTextBox(Factory, CalculatorWindow.RightArgumentTextBox, "Right Argument");
-            const string expectedValue = "1";
-            textBox.ClearAndType(expectedValue);
-            Assert.AreEqual(expectedValue, textBox.Value);
+            textBox.ClearAndType(ExpectedValue);
+            Assert.AreEqual(ExpectedValue, textBox.Value);
         }
 
         public static ITextBox GetTextBox(IElementFactory coreFactory, By loc, string nam) => coreFactory.GetTextBox(loc, nam);
