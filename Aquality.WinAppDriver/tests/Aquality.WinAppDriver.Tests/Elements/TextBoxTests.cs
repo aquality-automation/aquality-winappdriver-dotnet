27a744aa84f9c99fc4517f42b162cfe962cc2d92
Elena Suboch <korbrakit@gmail.com>
2019-09-17 11:23:16 +0300

    moved out TextBoxRightArgument from test methods

--- Aquality.WinAppDriver/tests/Aquality.WinAppDriver.Tests/Elements/TextBoxTests.cs
+++ Aquality.WinAppDriver/tests/Aquality.WinAppDriver.Tests/Elements/TextBoxTests.cs
@@ -9,41 +9,39 @@ namespace Aquality.WinAppDriver.Tests.Elements
     public class TextBoxTests : TestWithApplication
     {
         private const string ExpectedValue = "1";
-        private IElementFactory Factory => ApplicationManager.GetRequiredService<IElementFactory>();
+
+        private readonly ITextBox TextBoxRightArgument = 
+            GetTextBox(ApplicationManager.GetRequiredService<IElementFactory>(), CalculatorWindow.RightArgumentTextBox, "Right Argument");
 
         [Test]
         public void Should_EnterValues()
         {
-            var textBox = GetTextBox(Factory, CalculatorWindow.RightArgumentTextBox, "Right Argument");
-            textBox.Type(ExpectedValue);
-            Assert.AreEqual(ExpectedValue, textBox.Value);
+            TextBoxRightArgument.Type(ExpectedValue);
+            Assert.AreEqual(ExpectedValue, TextBoxRightArgument.Value);
         }
 
         [Test]
         public void Should_EnterValues_WithoutCleaningTextbox()
         {
-            var textBox = GetTextBox(Factory, CalculatorWindow.RightArgumentTextBox, "Right Argument");
-            textBox.Type(ExpectedValue);
+            TextBoxRightArgument.Type(ExpectedValue);
             const string additionalExpectedValue = "23";
-            textBox.Type(additionalExpectedValue);
-            Assert.AreEqual(ExpectedValue + additionalExpectedValue, textBox.Value);
+            TextBoxRightArgument.Type(additionalExpectedValue);
+            Assert.AreEqual(ExpectedValue + additionalExpectedValue, TextBoxRightArgument.Value);
         }
 
         [Test]
         public void Should_ClearTextBeforeEnteringValues_PrefilledTextbox()
         {
-            var textBox = GetTextBox(Factory, CalculatorWindow.RightArgumentTextBox, "Right Argument");
-            textBox.Type("123");
-            textBox.ClearAndType(ExpectedValue);
-            Assert.AreEqual(ExpectedValue, textBox.Value);
+            TextBoxRightArgument.Type("123");
+            TextBoxRightArgument.ClearAndType(ExpectedValue);
+            Assert.AreEqual(ExpectedValue, TextBoxRightArgument.Value);
         }
 
         [Test]
         public void Should_ClearTextBeforeEnteringValues_EmptyTextbox()
         {
-            var textBox = GetTextBox(Factory, CalculatorWindow.RightArgumentTextBox, "Right Argument");
-            textBox.ClearAndType(ExpectedValue);
-            Assert.AreEqual(ExpectedValue, textBox.Value);
+            TextBoxRightArgument.ClearAndType(ExpectedValue);
+            Assert.AreEqual(ExpectedValue, TextBoxRightArgument.Value);
         }
 
         public static ITextBox GetTextBox(IElementFactory coreFactory, By loc, string nam) => coreFactory.GetTextBox(loc, nam);
